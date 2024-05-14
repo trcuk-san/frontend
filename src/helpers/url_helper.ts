39c2401f44f0f5c3bbcb49d722395100b94b9ede
big555ddd <98012117+big555ddd@users.noreply.github.com>
2024-05-14 23:15:03 +0700

    test

--- src/helpers/url_helper.ts
+++ src/helpers/url_helper.ts
@@ -7,7 +7,7 @@ export const POST_FAKE_JWT_LOGIN = "/post-jwt-login";
 export const POST_FAKE_PASSWORD_FORGET = "/auth/forgot-password";
 export const POST_FAKE_JWT_PASSWORD_FORGET = "/jwt-forget-pwd";
 export const SOCIAL_LOGIN = "/social-login";
-
+export const GET_DEMO_DATA = "https://jsonplaceholder.typicode.com/posts";
 //PROFILE
 export const POST_EDIT_JWT_PROFILE = "/post-jwt-profile";
 export const POST_EDIT_PROFILE = "/user";
